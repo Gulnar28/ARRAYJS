843a01be6b319881dd70cbab658541c349c8026c
gulnarvma@outlook.com <gulnarvma@outlook.com>
2023-05-07 03:20:14 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+//     //  #TASK 1;
+
+//     let myArr =  [1,3,4,4];
+//       function findDuplicate(myArr){
+//         for(let i=0;i<myArr.length-1;i++){
+//             if (myArr[i]==myArr[i+1]){
+//               return myArr[i]; 
+//             } 
+//         }
+//       }
+// console.log(findDuplicate(myArr));
+
+
+
+  // #TASK 2;
+  
+      // let myArr = [1,2,3,4];
+      //   function findMax(myArr){
+      //       let max=myArr[0]
+      //   for (let i = 1; i < myArr.length; i++){
+      //     if (myArr[i] > max){
+      //       max = myArr[i];
+           
+      //     }
+      //   }
+      //   return max;
+      // }
+      // console.log(findMax(myArr));
+
+
+
+// // #TASK 3;
+
+      // let myArr = [1,2,3,4];
+      // function arrayHandler(myArr){
+      //   var newArray = [];
+      //   for (i = 0; i < myArr.length; i++) {
+      //    newArray.push(myArr[i] * 2);
+      //   }
+      
+      //   return newArray;
+      //  }
+      //  console.log(arrayHandler(myArr));
+
+
